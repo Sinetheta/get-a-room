03d1e75fa93f52c0c842b240a8ed4dc2b0f405ef
Kevin Attfield <kevin@stembolt.com>
2017-01-08 12:50:01 -0800

    Remove boilerplate intro

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
     <calendar-authorize></calendar-authorize>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
 import CalendarAuthorize from './components/calendar/authorize';
 
 export default {
   name: 'app',
   components: {
-    Hello,
     CalendarAuthorize
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
